35de55db2f2c5a475cbaf0c83faae92411d0a1e3
backgyuwon <167157762+backgyuwon@users.noreply.github.com>
2025-01-21 15:58:33 +0900

    code changes
    code changes

--- src/testPackage/testClass.java
+++ src/testPackage/testClass.java
@@ -3,7 +3,7 @@ package testPackage;
 public class testClass {
 
 	public static void main(String[] args) {
-		System.out.println("Test Java 134434");
+		System.out.println("Test Java for github");
 	}
 
 }
